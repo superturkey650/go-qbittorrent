ec6497aeaf3f18ae59d00708d6b039898f1eef8e
Jared Mosley <anmousyon@gmail.com>
2017-04-13 13:59:28 -0500

    added comments to main

--- main.go
+++ main.go
@@ -3,20 +3,30 @@ package main
 import (
 	"fmt"
 	"go-qbittorrent/qbt"
+	"go-qbittorrent/tools"
 )
 
 func main() {
+	// connect to qbittorrent client
 	qb := qbt.NewClient("http://localhost:8080/")
 
-	qb.Login("username", "password")
-
-	options := map[string]string{}
-	file := "/Users/me/Downloads/Source.Code.2011.1080p.BluRay.H264.AAC-RARBG-[rarbg.to].torrent"
-	_, err := qb.DownloadFromFile(file, options)
+	// login to the client
+	resp, err := qb.Login("username", "password")
 	if err != nil {
-		fmt.Println("error on downloading from file")
+		fmt.Println(err)
+	}
+
+	// were no using an filters so the options map is empty
+	options := map[string]string{}
+	// set the path to the file
+	file := "/Users/me/Downloads/Source.Code.2011.1080p.BluRay.H264.AAC-RARBG-[rarbg.to].torrent"
+	// download the torrent using the file
+	// the wrapper will handle opening and closing the file for you
+	resp, err := qb.DownloadFromFile(file, options)
+	if err != nil {
+		tools.PrintResponse(resp.Body)
 		fmt.Println(err)
 	} else {
-		fmt.Println("success")
+		fmt.Println("downloaded successful")
 	}
 }
